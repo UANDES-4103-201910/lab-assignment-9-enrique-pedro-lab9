87dc68aa4672f458a9c1062192972e3ef803489c
erriveros <erriveros@miuandes.cl>
2019-05-23 19:07:55 -0400

    part 2 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :events
+  resources :ticket_types
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_185518) do
+ActiveRecord::Schema.define(version: 2019_05_23_221249) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 2019_05_23_185518) do
     t.string "name"
     t.string "address"
     t.integer "capacity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ticket_types", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
