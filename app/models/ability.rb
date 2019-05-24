26aa9d1dea66a58522ad9c28e4660d4a8e1ff2d9
erriveros <erriveros@miuandes.cl>
2019-05-23 23:13:28 -0400

    merge

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,7 @@ class Ability
   def initialize(user)
     can :read, :all # permissions for every user, even if not logged in    
     if user.present?  # additional permissions for logged in users (they can manage their posts)
-      can :manage, user_ticket, user_id: user.id 
+      can :manage, user_ticket
       if user.admin?  # additional permissions for administrators
         can :manage, :all
       end
